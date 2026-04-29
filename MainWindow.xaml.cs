b4113196a37f5718f655639ecbac02ee04969c14
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2026-04-29 12:44:04 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Paleta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            b1.Background = new SolidColorBrush(Color.FromRgb(255, 255, 00));
+        }
+    }
+}
